7c745d6403d1582566f673412d96222431e55c19
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-03-26 20:03:26 +0300

    Update validator.php

--- lab3/validator.php
+++ lab3/validator.php
@@ -6,7 +6,7 @@ function is_valid_tel($tel) {
   return !empty($tel) && preg_match("/^(\+7|\+38)?\s?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/", $tel);
 }
 function is_valid_email($email) {
-  return !empty($email) && preg_match("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[ a-zA-Z0-9])?)*$/", $email);
+  return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 function is_valid_data_birth($birth) {
   return !empty($birth) && preg_match("/^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.(19|20)\d\d$/", $birth);
