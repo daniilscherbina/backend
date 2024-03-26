eb281c43e2c24eee616d1b6228e547d5b2083169
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-03-26 19:03:20 +0300

    Update form.php

--- lab3/form.php
+++ lab3/form.php
@@ -21,6 +21,7 @@
   <label for="biography">Биография:</label><br>
   <textarea id="biography" name="biography" rows="4" cols="50"></textarea><br><br>
 
+  <label for="lan">Любимые языки::</label>
   <select multiple size="10" name="lan" id="lan">
     <?php
     require_once "database.php";
