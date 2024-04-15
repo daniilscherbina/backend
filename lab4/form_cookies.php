75c02e19aac782355734d17ebf1be1d1b0d9d582
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-04-15 09:01:36 +0300

    Update form_cookies.php

--- lab4/form_cookies.php
+++ lab4/form_cookies.php
@@ -1,15 +1,15 @@
 <?php 
   function error_cookie_cl($name, $text, $errors) {
-    if ($errors[name]) {
+    if ($errors[$name]) {
       // Удаляем куки, указывая время устаревания в прошлом.
-      setcookie(name . '_error', '', 100000);
-      setcookie(name . '_value', '', 100000);
+      setcookie($name . '_error', '', 100000);
+      setcookie($name . '_value', '', 100000);
       // Выводим сообщение.
-      $messages[] = '<div class="error">' . text . '</div>';
+      $messages[] = '<div class="error">' . $text . '</div>';
     }
   }
 
   function values_set($name, $values) {
-    $values[name] = empty($_COOKIE[name . '_value']) ? '' : $_COOKIE[name . '_value'];
+    $values[$name] = empty($_COOKIE[$name . '_value']) ? '' : $_COOKIE[$name . '_value'];
   }
 ?>
