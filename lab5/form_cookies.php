96ca39cc36aa27798071a91b5b721a0d0e16dfe6
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-04-30 16:44:56 +0300

    Create form_cookies.php

---
+++ lab5/form_cookies.php
@@ -0,0 +1,15 @@
+<?php 
+  function error_cookie_cl($name, $text, &$errors, &$messages) {
+    if ($errors[$name]) {
+      // Удаляем куки, указывая время устаревания в прошлом.
+      setcookie($name . '_error', '', 100000);
+      setcookie($name . '_value', '', 100000);
+      // Выводим сообщение.
+      $messages[$name] = '<div class="error">' . $text . '</div>';
+    }
+  }
+
+  function values_set($name, &$values) {
+    $values[$name] = empty($_COOKIE[$name . '_value']) ? '' : $_COOKIE[$name . '_value'];
+  }
+?>
