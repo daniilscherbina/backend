94e7d56b8b7740d1d36f6135fc0197d2334e7dfa
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-04-30 16:46:15 +0300

    Update index.php

--- lab5/index.php
+++ lab5/index.php
@@ -2,6 +2,7 @@
 // Отправляем браузеру правильную кодировку,
 // файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
+header('Cache-Control: no-cache, must-revalidate');
 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
