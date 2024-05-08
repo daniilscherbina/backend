bd5e2464788407a2460c07508f1f613d9ff67d11
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-08 19:00:47 +0300

    Create dashboard.php

---
+++ lab6/dashboard.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: 'Trebuchet MS', Helvetica, sans-serif;
+            display: flex;
+            height: 100vh;
+            background-color: #101729;
+            margin: 0;
+            padding: 0;
+        }
+        .left-block {
+            padding-top: 75px;
+            display: flex;
+            flex-direction: column;
+            width: 250px;
+            background-color: #161d2f;
+            color: white;
+            a {
+                color: white;
+                padding: 15px;
+                padding-left: 25px;
+                font-size: 20px;
+                text-decoration: none;
+                transition: all 0.3s ease;
+            }
+
+            a:hover {
+                background-color: #1d283c;
+                border-right: 5px solid #2869ff;
+            }
+        }
+        .right-block {
+            flex-grow: 1;
+            display: flex;
+            flex-direction: column;
+        }
+        .top-subblock {
+            height: 80px;
+            display: flex;
+            padding: 25px;
+            justify-content: space-between;
+            background-color: #101729;
+            flex-direction: column;
+            .top-subblock-content {
+                display: flex;
+                justify-content: space-between;
+            }
+            .top-subblock-header {
+                color: white;
+                font-size: 26px;
+                margin-bottom: 25px;
+            }
+        }
+        .bottom-subblock {
+            flex-grow: 1;
+            overflow: auto;
+            padding: 25px;
+            background-color: #101729;
+        }
+        .add-button {
+            background-color: #2968ff;
+            color: white;
+            padding: 10px;
+            border: none;
+            border-radius: 10px;
+        }
+        .search-bar {
+            background-color: #1d283c;
+            color: white;
+            padding: 10px 25px;
+            border: none;
+            width: 250px;
+            border-radius: 10px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th {
+            background-color: #1d283c;
+            color: white;
+            padding: 10px 20px;
+            text-align: left;
+            margin-left: 5px;
+        }
+
+        th:first-child {
+            border-top-left-radius: 10px;
+            border-bottom-left-radius: 10px;
+        }
+
+        th:last-child {
+            border-top-right-radius: 10px;
+            border-bottom-right-radius: 10px;
+        }
+
+        td {
+            background-color: transparent;
+            color: white;
+            padding: 10px 25px;
+        }
+    </style>
+</head>
+<body>
+    <div class="right-block">
+        <div class="top-subblock">
+            <div class="top-subblock-header">
+                Данные опроса
+            </div>
+        </div>
+        <div class="bottom-subblock">
+            <table>
+                <tr>
+                    <th>Колонка 1</th>
+                    <th>Колонка 2</th>
+                    <th>Колонка 3</th>
+                    <th>Колонка 4</th>
+                    <th>Колонка 5</th>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+                <tr>
+                    <td>Данные 1</td>
+                    <td>Данные 2</td>
+                    <td>Данные 3</td>
+                    <td>Данные 4</td>
+                    <td>Данные 5</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
