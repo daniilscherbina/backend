cbfe5584b58894039e12ccf048372f718bf0df85
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-08 20:00:48 +0300

    Update admin.php

--- lab6/admin.php
+++ lab6/admin.php
@@ -11,7 +11,7 @@
 if (empty($_SERVER['PHP_AUTH_USER']) ||
     empty($_SERVER['PHP_AUTH_PW']) ||
     $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-    md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
+    md5($_SERVER['PHP_AUTH_PW']) != md5('50199')) {
   header('HTTP/1.1 401 Unanthorized');
   header('WWW-Authenticate: Basic realm="My site"');
   print('<h1>401 Требуется авторизация</h1>');
