028f6c7693b55f2594dd8335d240a6e552cd2f9c
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-08 18:49:11 +0300

    Create login.php

---
+++ lab6/login.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Файл login.php для не авторизованного пользователя выводит форму логина.
+ * При отправке формы проверяет логин/пароль и создает сессию,
+ * записывает в нее логин и id пользователя.
+ * После авторизации пользователь перенаправляется на главную страницу
+ * для изменения ранее введенных данных.
+ **/
+
+// Отправляем браузеру правильную кодировку,
+// файл login.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SESSION хранятся переменные сессии.
+// Будем сохранять туда логин после успешной авторизации.
+$session_started = false;
+if (!empty($_COOKIE) && $_COOKIE[session_name()] && session_start()) {
+  $session_started = true;
+  if (!empty($_SESSION['login'])) {
+    // Если есть логин в сессии, то пользователь уже авторизован.
+    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
+    //при нажатии на кнопку Выход).
+    // Делаем перенаправление на форму.
+    header('Location: ./');
+    exit();
+  }
+}
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Логин</title>
+    <link rel="stylesheet" href="login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <form action="" method="post">
+      <input name="login" placeholder="Логин"/>
+      <input name="pass" placeholder="Пароль"/>
+      <input type="submit" value="Войти" />
+    </form>
+  </body>
+</html>
+
+<?php
+}
+// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
+else {
+  // TODO: Проверть есть ли такой логин и пароль в базе данных.
+  // Выдать сообщение об ошибках.
+  
+  require_once "database.php";
+  $hash = md5($_POST['pass']);
+  if (!check_credentials($_POST['login'], $hash)) {
+    exit();
+  }
+
+  if (!$session_started) {
+    session_start();
+  }
+  // Если все ок, то авторизуем пользователя.
+  $_SESSION['login'] = $_POST['login'];
+  // Записываем ID пользователя.
+  $_SESSION['uid'] = $_POST['login'];
+
+  // Делаем перенаправление.
+  header('Location: ./');
+}
