c041c6ea682c1660cdc27cb42e57d23892fecc42
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 12:20:30 +0300

    Update routers_view.php

--- практика/routers_view.php
+++ практика/routers_view.php
@@ -7,7 +7,7 @@
   function build_mains_table_view($table_name) {
     global $tables;
     if(!in_array($table_name, $tables)) {
-      print_error_content()
+      print_error_content();
     } else {
       include('table_template_view.php');
     }
