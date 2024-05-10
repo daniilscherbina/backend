8113f2dc87573adab3d064c7e86c487c618fc6b3
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 14:39:48 +0300

    Create all_request.php

---
+++ practic/all_request.php
@@ -0,0 +1,16 @@
+<div class="top-subblock">
+    <div class="top-subblock-header">
+        <?php print('Список всех запросов'); ?>
+    </div>
+</div>
+<div class="bottom-subblock">
+    <a href="?request=1">Запрос 1.</a>
+    <a href="?request=2">Запрос 2.</a>
+    <a href="?request=3">Запрос 3.</a>
+    <a href="?request=4">Запрос 4.</a>
+    <a href="?request=5">Запрос 5.</a>
+    <a href="?request=6">Запрос 6.</a>
+    <a href="?request=7">Запрос 7.</a>
+    <a href="?request=8">Запрос 8.</a>
+    <a href="?request=9">Запрос 9. Выбирает информацию о VIP клиентах, для которых процент скидки равен 0.5%</a>
+</div>
