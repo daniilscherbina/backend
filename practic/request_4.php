071f073ca12f77294f0bf9e377ea479bb6366e0d
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 18:24:51 +0300

    Update request_4.php

--- practic/request_4.php
+++ practic/request_4.php
@@ -17,9 +17,7 @@
     <table>
         <?php
             require_once "database.php";
-            if (isset($_COOKIE['request4'])) {
-                print($_COOKIE['request4']);
-            }
+            
         ?>
     </table>
 </div>
