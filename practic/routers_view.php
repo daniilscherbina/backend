1dda8d39a2d45c5f5c406a45af369855ecebad08
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 14:12:19 +0300

    Update routers_view.php

--- practic/routers_view.php
+++ practic/routers_view.php
@@ -19,6 +19,7 @@
       }
     },
     'request' => function ($request_num) {
+      global $requests_discription;
       if(!in_array($request_num, $requests_discription)) {
         print_error_content();
       } else {
