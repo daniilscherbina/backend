7f7fabb2f51ed754701666feb95a652a9051508c
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 18:49:55 +0300

    Update request_4.php

--- practic/request_4.php
+++ practic/request_4.php
@@ -20,32 +20,33 @@
             global $post;
             if (isset($post['request4'])) {
                 $result = get_result_4($post['request4']);
-
-                $rows = $result->fetchAll(PDO::FETCH_ASSOC);
-    
-                $columnCount = $result->columnCount();
-                $columnNames = array();
-                for ($i = 0; $i < $columnCount; $i++) {
-                    $columnMeta = $result->getColumnMeta($i);
-                    $columnNames[] = $columnMeta['name'];
-                }
-    
-                print('<tr>');
-                foreach ($columnNames as $name) {
-                  print('<th>');
-                    print($name);
-                  print('</th>');
-                }
-                print('</tr>');
-    
-                foreach ($rows as $row) {
+                if(!is_null($result)) {
+                  $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+      
+                  $columnCount = $result->columnCount();
+                  $columnNames = array();
+                  for ($i = 0; $i < $columnCount; $i++) {
+                      $columnMeta = $result->getColumnMeta($i);
+                      $columnNames[] = $columnMeta['name'];
+                  }
+      
                   print('<tr>');
-                    foreach ($row as $col) {
-                        print('<td>');
-                            print($col);
-                        print('</td>');
-                    }
+                  foreach ($columnNames as $name) {
+                    print('<th>');
+                      print($name);
+                    print('</th>');
+                  }
                   print('</tr>');
+      
+                  foreach ($rows as $row) {
+                    print('<tr>');
+                      foreach ($row as $col) {
+                          print('<td>');
+                              print($col);
+                          print('</td>');
+                      }
+                    print('</tr>');
+                  }
                 }
             }
         ?>
