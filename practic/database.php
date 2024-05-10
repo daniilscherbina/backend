1df9efb3dba4592e92f33115d3862033411b5a3a
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 14:05:32 +0300

    Update database.php

--- practic/database.php
+++ practic/database.php
@@ -18,18 +18,23 @@ function get_table_colown_names($table_name) {
 
 function get_all_rows_table($table_name) {
   $db = create_db_connection();
-    $query = $db->prepare("SELECT * FROM " . $table_name);
-    $query->execute();
-    $rows = $rows = $query->fetchAll(PDO::FETCH_ASSOC);
-    return $rows;
+  $query = $db->prepare("SELECT * FROM " . $table_name);
+  $query->execute();
+  $rows = $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+  return $rows;
 }
 
 $requests = array(
-    '9' => function($request_num) {
-        $query = $db->prepare("SELECT * FROM clients WHERE discount = 0.5;");
-        $query->execute();
-        return $query;
-    }
+  '9' => function($request_num) {
+    $db = create_db_connection();
+    $query = $db->prepare("SELECT * FROM clients WHERE discount = 0.5;");
+    $query->execute();
+    return $query;
+  }
+);
+
+$requests_discription = array(
+  '9' => 'Выбирает информацию о VIP клиентах, для которых процент скидки равен 0.5%'
 );
 
 ?>
