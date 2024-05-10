a674779420c40dd835d7408b6cebdeff4abfb621
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 11:44:37 +0300

    Create index.php

---
+++ практика/index.php
@@ -0,0 +1,12 @@
+<?php
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+    empty($_SERVER['PHP_AUTH_PW']) ||
+    $_SERVER['PHP_AUTH_USER'] != 'admin' ||
+    md5($_SERVER['PHP_AUTH_PW']) != md5('50199')) {
+  header('HTTP/1.1 401 Unanthorized');
+  header('WWW-Authenticate: Basic realm="My site"');
+  print('<h1>401 Требуется авторизация</h1>');
+  exit();
+}
+
+include('dashboard.php');
