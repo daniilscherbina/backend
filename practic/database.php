71cc6836a8c3a557b91c64012ff79df84ce74acf
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 15:34:33 +0300

    Update database.php

--- practic/database.php
+++ practic/database.php
@@ -24,36 +24,31 @@ function get_all_rows_table($table_name) {
   return $rows;
 }
 
+function get_result($str) {
+    $db = create_db_connection();
+    $query = $db->prepare($str);
+    $query->execute();
+    return $query;
+  },
+
 $requests = array(
   '9' => function() {
-    $db = create_db_connection();
-    $query = $db->prepare("SELECT last_name as 'Фамилия', first_name as 'Имя', patronymic as 'Отчество', discount as 'Персональная скидка %' FROM clients WHERE discount = 0.5;");
-    $query->execute();
-    return $query;
+    return get_result("SELECT last_name as 'Фамилия', first_name as 'Имя', patronymic as 'Отчество', discount as 'Персональная скидка %' FROM clients WHERE discount = 0.5;");
   },
   '8' => function() {
-    $db = create_db_connection();
-    $query = $db->prepare("SELECT date AS 'Дата подписания договора', MIN(amount_of_insurance) AS 'Минимальная сумма страхования', MAX(amount_of_insurance) AS 'Максимальная сумма страхования' FROM contracts GROUP BY date;");
-    $query->execute();
-    return $query;
+    return get_result("SELECT date AS 'Дата подписания договора', MIN(amount_of_insurance) AS 'Минимальная сумма страхования', MAX(amount_of_insurance) AS 'Максимальная сумма страхования' FROM contracts GROUP BY date;");
   },
   '6' => function() {
-    $db = create_db_connection();
-    $query = $db->prepare("SELECT c.date AS 'Дата подписания договора', CONCAT(cl.first_name, ' ', cl.last_name, ' ', cl.patronymic) AS 'Ф.И.О. клиента', c.amount_of_insurance AS 'Сумма страхования', (amount_of_insurance * ((SELECT tariff FROM insurance_type WHERE id = c.id_insurance_type) - (SELECT discount FROM clients WHERE id = c.id_clients)) / 100) AS 'Размер страховой премии' FROM contracts c JOIN clients cl ON c.id_clients = cl.id;");
-    $query->execute();
-    return $query;
+    return get_result("SELECT c.date AS 'Дата подписания договора', CONCAT(cl.first_name, ' ', cl.last_name, ' ', cl.patronymic) AS 'Ф.И.О. клиента', c.amount_of_insurance AS 'Сумма страхования', (amount_of_insurance * ((SELECT tariff FROM insurance_type WHERE id = c.id_insurance_type) - (SELECT discount FROM clients WHERE id = c.id_clients)) / 100) AS 'Размер страховой премии' FROM contracts c JOIN clients cl ON c.id_clients = cl.id;");
   },
   '4' => function() {
-    $db = create_db_connection();
-    $query = $db->prepare("SELECT ia.id AS 'Код агента', CONCAT(ia.first_name, ' ', ia.last_name, ' ', ia.patronymic) AS 'Ф.И.О. агента',  c.id AS 'Код контракта', c.date AS 'Дата подписания контракта', CONCAT(cl.first_name, ' ', cl.last_name, ' ', cl.patronymic) AS 'Ф.И.О. клиента', c.amount_of_insurance AS 'Сумма страхования', it.name AS 'Тип страхования' FROM insurance_agents ia JOIN contracts c ON ia.id = c.id_insurance_agent JOIN insurance_type it ON it.id = c.id_insurance_type JOIN clients cl ON cl.id = c.id_clients WHERE c.amount_of_insurance >= 200000;");
-    $query->execute();
-    return $query;
+    return get_result("SELECT ia.id AS 'Код агента', CONCAT(ia.first_name, ' ', ia.last_name, ' ', ia.patronymic) AS 'Ф.И.О. агента',  c.id AS 'Код контракта', c.date AS 'Дата подписания контракта', CONCAT(cl.first_name, ' ', cl.last_name, ' ', cl.patronymic) AS 'Ф.И.О. клиента', c.amount_of_insurance AS 'Сумма страхования', it.name AS 'Тип страхования' FROM insurance_agents ia JOIN contracts c ON ia.id = c.id_insurance_agent JOIN insurance_type it ON it.id = c.id_insurance_type JOIN clients cl ON cl.id = c.id_clients WHERE c.amount_of_insurance >= 200000;");
   },
   '2' => function() {
-    $db = create_db_connection();
-    $query = $db->prepare("SELECT ia.id AS 'Код агента', CONCAT(ia.first_name, ' ', ia.last_name, ' ', ia.patronymic) AS 'Ф.И.О. агента', ia.bet AS 'Процент вознаграждения' FROM ia WHERE bet >= 20 AND bet <= 50;");
-    $query->execute();
-    return $query;
+    return get_result("SELECT ia.id AS 'Код агента', CONCAT(ia.first_name, ' ', ia.last_name, ' ', ia.patronymic) AS 'Ф.И.О. агента', ia.bet AS 'Процент вознаграждения' FROM ia WHERE bet >= 20 AND bet <= 50;");
+  },
+  '1' => function() {
+    return get_result("SELECT cl.id AS 'Код клиента', CONCAT(cl.first_name, ' ', cl.last_name, ' ', cl.patronymic) AS 'Ф.И.О. клиента', cl.discount AS 'Персональная скидка' FROM clients cl WHERE last_name = 'Иванов';
   },
 );
 
