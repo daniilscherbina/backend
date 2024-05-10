8e3e6f55569c0891dc7299e3a809a77093a6dc28
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 14:23:04 +0300

    Update request_table_template.php

--- practic/request_table_template.php
+++ practic/request_table_template.php
@@ -1,12 +1,15 @@
 <div class="top-subblock">
     <div class="top-subblock-header">
-        <?php print('Запрос под номером: ' . $request_num . '; ' . $requests_discription[$request_num]); ?>
+        <?php print('Запрос под номером: ' . $request_num); ?>
     </div>
     <div class="top-subblock-content">
         <input class="search-bar" type="text" placeholder="Поиск">
     </div>
 </div>
 <div class="bottom-subblock">
+    <div>
+        <?php print($requests_discription[$request_num]); ?>
+    </div>
     <table>
         <?php
             require_once "database.php";
