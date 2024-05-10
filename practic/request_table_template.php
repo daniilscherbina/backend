72d69360c2826fa4bbb15d5577048a50aa3af5ca
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 14:29:49 +0300

    Update request_table_template.php

--- practic/request_table_template.php
+++ practic/request_table_template.php
@@ -10,7 +10,35 @@
     <table>
         <?php
             require_once "database.php";
-            
+
+            $result = $requests[$request_num]();
+
+            $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+
+            $columnCount = $result->columnCount();
+            $columnNames = array();
+            for ($i = 0; $i < $columnCount; $i++) {
+                $columnMeta = $result->getColumnMeta($i);
+                $columnNames[] = $columnMeta['name'];
+            }
+
+            print('<tr>');
+            foreach ($columnNames as $name) {
+                    print('<th>');
+                        print($name);
+                    print('</th>');
+            }
+            print('</tr>');
+
+            foreach ($rows as $row) {
+                print('<tr>');
+                    foreach ($row as $col) {
+                        print('<td>');
+                            print($col);
+                        print('</td>');
+                    }
+                print('</tr>');
+            }
         ?>
     </table>
 </div>
