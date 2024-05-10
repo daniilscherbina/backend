370c78d8a02679d55f103d93b4bba8d90f48b64e
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 12:33:48 +0300

    Update routers_view.php

--- практика/routers_view.php
+++ практика/routers_view.php
@@ -1,15 +1,33 @@
 <?php
-  $tables = ['insurance_type', 'clients', 'insurance_agents', 'contracts'];
-
   function print_error_content() {
     print('Некорректные параметры в url');
   }
-  function build_mains_table_view($table_name) {
-    global $tables;
-    if(!in_array($table_name, $tables)) {
-      print_error_content();
+
+  $tables = ['insurance_type', 'clients', 'insurance_agents', 'contracts'];
+
+  $views = array(
+    'table' => function build_mains_table_view($table_name) {
+      global $tables;
+      if(!in_array($table_name, $tables)) {
+        print_error_content();
+      } else {
+        include('table_template_view.php');
+      }
+    }
+  );
+
+  function open_view($parameters) {
+    if (!empty($parameters)) {
+        if (count($parameters) == 1) {
+            global $views;
+            if (isset($views[array_key_first($parameters)])) {
+                $views[array_key_first($parameters)]($parameters[array_key_first($parameters)]);
+            } else {
+                print_error_content();
+            }
+        }
     } else {
-      include('table_template_view.php');
+        print_error_content();
     }
   }
 ?>
