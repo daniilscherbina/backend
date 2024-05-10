9d1607d4bd6f2e3f4508497f51b98f99c08632bb
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 13:13:41 +0300

    Update database.php

--- practic/database.php
+++ practic/database.php
@@ -2,8 +2,7 @@
 require_once "../secret.php";
 print($user);
 function create_db_connection() {
-    global $user;
-    global $pass;
+
     return new PDO('mysql:host=localhost;dbname=' . $user, $user, $pass,
                     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
