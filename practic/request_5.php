1441b7ad733ae2cd13027cd5379b3349e7e9a3f1
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 18:47:49 +0300

    Create request_5.php

---
+++ practic/request_5.php
@@ -0,0 +1,53 @@
+<div class="top-subblock">
+    <div class="top-subblock-header">
+        Запрос под номером: 5
+    </div>
+    <div class="top-subblock-content">
+        <form action="" method="POST">
+            <input class="search-bar" type="hidden" id="request" name="request" value="5">
+            <input class="search-bar" type="text" id="last_name" name="last_name" placeholder="Фамилия страхового агента">
+            <input type="submit" class="add-button"  value="Выполнить запрос">
+        </form>
+    </div>
+</div>
+<div class="bottom-subblock">
+    <div class="discription">
+       Выбирает из таблицы АГЕНТЫ информацию о страховых агентах с некоторой фамилией. Фамилия вводится пользователем.
+    </div>
+    <table>
+        <?php
+            require_once "database.php";
+            global $post;
+            if (isset($post['request4'])) {
+                $result = get_result_4($post['request4']);
+
+                $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+    
+                $columnCount = $result->columnCount();
+                $columnNames = array();
+                for ($i = 0; $i < $columnCount; $i++) {
+                    $columnMeta = $result->getColumnMeta($i);
+                    $columnNames[] = $columnMeta['name'];
+                }
+    
+                print('<tr>');
+                foreach ($columnNames as $name) {
+                  print('<th>');
+                    print($name);
+                  print('</th>');
+                }
+                print('</tr>');
+    
+                foreach ($rows as $row) {
+                  print('<tr>');
+                    foreach ($row as $col) {
+                        print('<td>');
+                            print($col);
+                        print('</td>');
+                    }
+                  print('</tr>');
+                }
+            }
+        ?>
+    </table>
+</div>
