ac4905a28fb5baec803420c10e0a9f42bdd127e8
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 18:42:24 +0300

    Update request_4.php

--- practic/request_4.php
+++ practic/request_4.php
@@ -19,7 +19,34 @@
             require_once "database.php";
             global $post;
             if (isset($post['request4'])) {
-                print($post['request4']);
+                $result = get_result_request_4($post['request4']);
+
+                $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+    
+                $columnCount = $result->columnCount();
+                $columnNames = array();
+                for ($i = 0; $i < $columnCount; $i++) {
+                    $columnMeta = $result->getColumnMeta($i);
+                    $columnNames[] = $columnMeta['name'];
+                }
+    
+                print('<tr>');
+                foreach ($columnNames as $name) {
+                  print('<th>');
+                    print($name);
+                  print('</th>');
+                }
+                print('</tr>');
+    
+                foreach ($rows as $row) {
+                  print('<tr>');
+                    foreach ($row as $col) {
+                        print('<td>');
+                            print($col);
+                        print('</td>');
+                    }
+                  print('</tr>');
+                }
             }
         ?>
     </table>
