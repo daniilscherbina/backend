ec11ae24e616328e57c696d728dc1ee8b4f8e4af
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 12:07:29 +0300

    Update database.php

--- практика/database.php
+++ практика/database.php
@@ -6,4 +6,27 @@ function create_db_connection() {
     return new PDO('mysql:host=localhost;dbname=' . $user, $user, $pass,
                     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
+
+function get_table_colown_names($table_name) {
+  $db = create_db_connection();
+  $result = $db->query("SELECT * FROM " . $table_name);
+  $columns = $result->fetch_fields();
+  $column_names = array();
+  foreach ($columns as $column) {
+    $column_names[] = $column->name;
+  }
+  return $column_names;
+}
+
+function get_all_rows_table($table_name) {
+  $db = create_db_connection();
+    $query = $db->prepare("SELECT * FROM" . $table_name);
+    $query->execute();
+    $rows = array();
+    while ($row = $query->fetch()) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
 ?>
