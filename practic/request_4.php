8285890d3413a6065ca41069085e61efede4322d
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-10 18:15:25 +0300

    Update request_4.php

--- practic/request_4.php
+++ practic/request_4.php
@@ -4,7 +4,8 @@
     </div>
     <div class="top-subblock-content">
         <form action="" method="POST">
-            <input class="search-bar" type="text" placeholder="Фамилия страхового агента">
+            <input class="search-bar" type="hidden" id="request" name="request" value="4">
+            <input class="search-bar" type="text" id="last_name" name="last_name" placeholder="Фамилия страхового агента">
             <input type="submit" class="add-button"  value="Выполнить запрос">
         </form>
     </div>
@@ -16,9 +17,6 @@
     <table>
         <?php
             require_once "database.php";
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                print('post');
-            }
         ?>
     </table>
 </div>
