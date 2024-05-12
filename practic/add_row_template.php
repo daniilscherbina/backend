324dc7b335a9d5abb15f5aa5e9a7044ac0cd7655
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-12 11:00:30 +0300

    Update add_row_template.php

--- practic/add_row_template.php
+++ practic/add_row_template.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Добавить новую запись в таблицу: <?php print($table); ?></title>
-    <link rel="stylesheet" href="../lab6/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
-  </head>
-  <body>
-    <form action="" method="POST">
-      <h1>Добавить новую запись в таблицу: <?php print($table); ?></h1>
-      <label for="fio">ФИО:</label>
-      <input type="text" id="fio" name="fio"><br><br>
-      
-      <input type="submit" value="Отправить">
-    </form>
-  </body>
-</html>
+<form action="" method="POST">
+  <h1>Добавить новую запись в таблицу: <?php print($table); ?></h1>
+  <label for="fio">ФИО:</label>
+  <input type="text" id="fio" name="fio"><br><br>
+  
+  <input type="submit" value="Отправить">
+</form>
