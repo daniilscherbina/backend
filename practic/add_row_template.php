fe087b2289a04cde7b34872d63114bea066c4052
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-12 11:05:28 +0300

    Update add_row_template.php

--- practic/add_row_template.php
+++ practic/add_row_template.php
@@ -1,7 +1,8 @@
 <form action="" method="POST">
   <h1>Добавить новую запись в таблицу: <?php print($table); ?></h1>
-  <label for="fio">ФИО:</label>
-  <input type="text" id="fio" name="fio"><br><br>
   
-  <input type="submit" value="Отправить">
+  <label for="fio">ФИО:</label>
+  <input type="text" id="fio" name="fio" class="search-bar"><br><br>
+  
+  <input type="submit" class="add-button" value="Отправить">
 </form>
