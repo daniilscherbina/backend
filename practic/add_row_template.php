9fab3c7452241fdc440d0ca086717d52e4b31be5
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-12 11:18:11 +0300

    Update add_row_template.php

--- practic/add_row_template.php
+++ practic/add_row_template.php
@@ -1,6 +1,6 @@
 <form action="" method="POST" class="new_row_form">
   <h1>Добавить новую запись в таблицу: <?php print($table); ?></h1>
-  <input class="search-bar" type="hidden" id="add_new_row" name="add_new_row" value="' . $table . '">
+  print('<input class="search-bar" type="hidden" id="add_new_row" name="add_new_row" value="' . $table . '">');
   <?php
     require_once "database.php";
     $names = get_table_colown_names($table);
