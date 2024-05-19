25144bcf32119cf4883f561f01127a2023bd1882
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-19 09:55:52 +0300

    router?

--- lab6/index.php
+++ lab6/index.php
@@ -1,12 +1,38 @@
 <?php
 
-header('Content-Type: text/html; charset=UTF-8');
-header('Cache-Control: no-cache, must-revalidate');
+  header('Content-Type: text/html; charset=UTF-8');
+  header('Cache-Control: no-cache, must-revalidate');
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  include "page_handlers/index/get_handler.php";
-  exit();
-}
+  function error_content() {
+    header('Location: ./?page=form');
+    exit();
+  }
 
-include "page_handlers/index/post_handler.php";
+  $views = array(
+    'form',
+    'admin',
+    'login',
+  );
+
+  function open_view($parameters) {
+    if (!empty($parameters)) {
+        if (count($parameters) == 1) {
+            global $views;
+            if (isset($parameters['page']) && isset($views[$parameters['page']])) {
+              if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                include "page_handlers/" . $parameters['page'] . "/get_handler.php";
+                exit();
+              }
+              
+              include "page_handlers/" . $parameters['page'] . "/post_handler.php";
+            } else {
+                error_content();
+            }
+        }
+    } else {
+        error_content();
+    }
+  }
+
+  open_view($_GET);
 ?>
