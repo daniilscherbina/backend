1d0d7efa2825740a19059da6a51291ca7a3e3ad6
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-22 17:07:45 +0300

    Update admin.php

--- lab6/admin.php
+++ lab6/admin.php
@@ -3,14 +3,21 @@
 // Пример HTTP-аутентификации.
 // PHP хранит логин и пароль в суперглобальном массиве $_SERVER.
 // Подробнее см. стр. 26 и 99 в учебном пособии Веб-программирование и веб-сервисы.
-if (empty($_SERVER['PHP_AUTH_USER']) ||
-    empty($_SERVER['PHP_AUTH_PW']) ||
-    $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-    md5($_SERVER['PHP_AUTH_PW']) != md5('50199')) {
-  header('HTTP/1.1 401 Unanthorized');
-  header('WWW-Authenticate: Basic realm="My site"');
-  print('<h1>401 Требуется авторизация</h1>');
-  exit();
+require_once "utils/database.php";
+try {
+    if (empty($_SERVER['PHP_AUTH_USER']) ||
+        empty($_SERVER['PHP_AUTH_PW']) ||
+        check_admin($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])
+       ) {
+      header('HTTP/1.1 401 Unanthorized');
+      header('WWW-Authenticate: Basic realm="My site"');
+      print('<h1>401 Требуется авторизация</h1>');
+      exit();
+    }
+}
+    catch(PDOException $e){
+    print('Error');
+    exit();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
