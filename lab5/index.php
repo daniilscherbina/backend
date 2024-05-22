74c4fdeed1c626daf5bcc53895a6727edc204a0e
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-22 18:26:51 +0300

    Update index.php

--- lab5/index.php
+++ lab5/index.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_SESSION['login'])) {
     require_once "utils/database.php";
     try {
-          $values = get_all_user_info();
+          $values = get_user($_SESSION['login']);
       } catch(PDOException $e){
         print('Error');
         exit();
