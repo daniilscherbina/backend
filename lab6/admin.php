870bdcccb36a84668c75b2307879b2b5891c4241
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2024-05-26 21:51:39 +0300

    Update admin.php

--- lab6/admin.php
+++ lab6/admin.php
@@ -22,23 +22,7 @@ try {
 
 session_start();
 
-$session_lifetime = 300; // 5 минут
-
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $session_lifetime) {
-    session_unset();
-    session_destroy();
-    unset($_SERVER['PHP_AUTH_USER']);
-    unset($_SERVER['PHP_AUTH_PW']);
-    print("Сессия устарела. Пожалуйста, войдите снова.");
-    exit();
-}
-
-// Обновление времени последней активности
-$_SESSION['last_activity'] = time();
-
-if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-}
+include "utils/csrf.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   include "page_handlers/admin/get_handler.php";
